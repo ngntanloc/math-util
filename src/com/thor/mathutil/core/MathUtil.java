70f722e82d4b3e7a68d0f3a58ae807f8dc70f546
ngtanloc <nguyentanloc160201@gmail.com>
2021-10-01 15:43:56 +0700

    add recursion to demonstrate regression testing. GREEN 4EVER again

--- src/com/thor/mathutil/core/MathUtil.java
+++ src/com/thor/mathutil/core/MathUtil.java
@@ -10,29 +10,50 @@ package com.thor.mathutil.core;
  * @author Nguyen Tan Loc
  */
 public class MathUtil {
+    
+    // viết theo kiểu đệ quy - recursion
+    // gọi lại chính mình với quy mô khác
+    // búp bê ng Nga
+    // 5! = 1 x 2 x 3 x 4 x  5
+    // 5! = 5 x 4!
+    // 4! = 4 x 3!
+    // 3! = 3 x 2!
+    // 2! = 2 x 1!
+    // 1! = 1 -> điểm dừng
+    // n! = n x (n-1)!
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20)
+            throw new IllegalArgumentException("Invalid arg, n must be between 0..20");
+        
+        if (n == 0 || n == 1)
+            return 1; // điểm dừng
+        
+        return n * getFactorial(n-1);
+        
+    }
 
     // viết hàm tính giai thừa n! = 1.2.3.4....n
     // 0! - 1! = 1
     // 21! - tràn kiểu long
     // <0! ko tính đc, ko có giai thừa cho số âm
-    public static long getFactorial(int n) {
-
-        if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("n must be between 0..20");
-        }
-
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-
-        long product = 1; // tích dồn
-
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-
-        return product;
-    }
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        }
+//
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//
+//        long product = 1; // tích dồn
+//
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//
+//        return product;
+//    }
 }
 
 // TDD - Test Driven Development
